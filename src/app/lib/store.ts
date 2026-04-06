dd4b20ea1a0317fcacae0a819d047e18fd443dcc
GyanaprakasKhandual <gyanaprakashkhnadual@gmail.com>
2026-04-06 21:06:22 +0530

    Vercel store issue fixed

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -3,9 +3,6 @@ import musicReducer from "./features/music/music.slice";
 import likesReducer from "./features/likes/like.slice";
 import commentsReducer from "./features/comments/comment.slice";
 import skillsReducer from "./features/skill/skill.slice";
-import blogsReducer from "./features/blogs/blog.slice"
-import experienceReducer from "./features/experience/experience.slice";
-import educationReducer from "./features/education/education.slice";
 import projectsReducer from "./features/projects/projects.slice";
 import contactReducer from "./features/contact/contact.slice";
 
@@ -15,9 +12,6 @@ export const store = configureStore({
     likes: likesReducer,
     comments: commentsReducer,
     skills: skillsReducer,
-    blogs: blogsReducer,
-    experience: experienceReducer,
-    education: educationReducer,
     projects: projectsReducer,
     contact: contactReducer,
   },
